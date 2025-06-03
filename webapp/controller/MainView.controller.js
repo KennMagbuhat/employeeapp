76500f4263747f56c5531a7bb4f56f3d0ca68275
Katherine Cabrito <katherine.cabrito@accenture.com>
2025-06-03 01:59:11 +0000

    Kath Initial Commit

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,10 +1,21 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ], (Controller) => {
     "use strict";
 
     return Controller.extend("sapips.training.employeeapp.controller.MainView", {
         onInit() {
+        },
+
+        onAdd: function () {
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            MessageToast.show(oBundle.getText("msgAddClicked"));
+        },
+      
+        onDelete: function () {
+          var oBundle = this.getView().getModel("i18n").getResourceBundle();
+          MessageToast.show(oBundle.getText("msgDeleteClicked"));
         }
     });
 });
