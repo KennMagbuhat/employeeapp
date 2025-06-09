4a2259cff03f844df1a7ef2f0fbd009acbecefa9
Christian jerald Chan <cxtronica.work@gmail.com>
2025-06-09 02:04:32 +0000

    still fixing

--- webapp/controller/EmployeeView.controller.js
+++ webapp/controller/EmployeeView.controller.js
@@ -10,32 +10,12 @@ sap.ui.define([
           var oModel = new JSONModel(sap.ui.require.toUrl("sapips/training/employeeapp/localService/mainService/data/EmployeeList.json"));
           this.getView().setModel(oModel);  // default model
 
-          let oSkillModel = new JSONModel(sap.ui.require.toUrl("sapips/training/employeeapp/localService/mainService/data/Skill.json"));
-          this.getView().setModel(oSkillModel, "skill");
+          var oSkillModel = new JSONModel(sap.ui.require.toUrl("sapips/training/employeeapp/localService/mainService/data/Skill.json"));
+          this.getView().setModel(oSkillModel, "empSkills");
 
-          //get router to pass data
           let oRouter = this.getOwnerComponent().getRouter();
-          oRouter.getRoute("RouteEmployeeView").attachPatternMatched(this._onObjectMatched, this);
+          console.log(oRouter);
 
-          //set the skills table title.
-          /**let oTitle = this.getView().byId("skillsTableTitleId");
-          const oTable = this.getView().byId("skillsTableEmpView");
-          const oItems = oTable.getBinding("items");
-          let sktext = oTitle.getText();
-          let listlength = Object.keys(oItems.oList).length;
-          console.log(oItems.oList);
-          sktext += "(" + listlength + ")";
-          oTitle.setText(sktext);*/
-        },
-
-        _onObjectMatched(oEvent){
-          let oModel = this.getOwnerComponent().getModel();
-          let args = oEvent.getParameter("arguments");
-          let eid_arg = args["employeeID"];
-          let sReadUri = oModel.createKey("/", {
-            EmployeeID: eid_arg
-          });
-          
 
         },
 
